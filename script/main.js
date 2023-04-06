3c9a23eb9f24b8d45a3f4989d99baecfbb4391eb
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-04-05 19:18:44 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -82,3 +82,21 @@
     x: gfx.tileW * 3,
     y: gfx.tileH * 5,
     speed: 4,
+    update: function() {
+      if (keys.left) {
+        this.x -= this.speed;
+      }
+      if (keys.right) {
+        this.x += this.speed;
+      }
+      if (keys.down) {
+        this.y += this.speed;
+      }
+      if (keys.up) {
+        return this.y -= this.speed;
+      }
+    },
+    render: function(gfx) {
+      return gfx.drawSprite(0, 0, this.x, this.y);
+    }
+  };
